48b75b81c8b030bca9e123a7959b58a6ca327859
AmanAswal <amanpotato11@gmail.com>
2025-02-05 11:34:57 +0530

    initial commit

---
+++ L1/app.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = 2;
+var n2 = 20;
+var ans = add(n1, n2);
+console.log(ans);
---
+++ L1/app.ts
@@ -0,0 +1,9 @@
+function add(num1: number, num2: number) {
+	return num1 + num2;
+}
+
+const n1 = 2;
+const n2 = 20;
+
+const ans = add(n1, n2);
+console.log(ans);
